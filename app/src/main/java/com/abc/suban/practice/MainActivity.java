a0b67657e401fbacccb53d012f5566c7a85806d0
Suban Dhyako <Suban Dhyako>
2016-05-08 08:24:01 +0545

    second abc

--- app/src/main/java/com/abc/suban/practice/MainActivity.java
+++ app/src/main/java/com/abc/suban/practice/MainActivity.java
@@ -2,6 +2,7 @@ package com.abc.suban.practice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this is git test
+        //aasdas
+        // asadasd
+        /// aaasss
+        ///aasds
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("check", "onStart");
+        //hello
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
 }
